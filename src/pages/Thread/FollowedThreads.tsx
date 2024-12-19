58d5c4e4d2681c737ce1efd7dd89dfa0c9da4ef6
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2024-12-19 11:29:18 +0700

    fix followed threads page

--- src/pages/Thread/FollowedThreads.tsx
+++ src/pages/Thread/FollowedThreads.tsx
@@ -1,50 +1,18 @@
-import { useState } from "react";
-import { ThreadSorter } from "../../components/Common/Sorter";
-import { ThreadCard } from "../../components/Category/ThreadCard";
-import Pagination from "../../components/Common/Pagination";
-import Loading from "../../components/Common/Loading";
-import { useThreads } from "../../hooks/useThreads";
+import FollowedThreadList from "../../components/User/FollowedThreadList";
 
-interface FollowedThreadListProps {
+interface FollowedThreadsProp {
   userId: number;
 }
-
-const FollowedThreadList: React.FC<FollowedThreadListProps> = ({ userId }) => {
-  const [sortBy, setSortBy] = useState<string>("created_at");
-  const [page, setPage] = useState<number>(1);
-
-  const token = localStorage.getItem("token");
-
-  const { threads, loading } = useThreads({
-    apiEndpoint: `/api/followed-threads/${userId}`,
-    params: { sort_by: sortBy, page: page, per_page: 5 },
-    token: token || "",
-  });
-
+const FollowedThreads: React.FC<FollowedThreadsProp> = ({ userId }) => {
   return (
-    <div className="container mx-auto px-4 py-6">
-      <ThreadSorter sortBy={sortBy} onSortChange={setSortBy} />
-
-      {loading && <Loading />}
-
-      {!loading && threads.length === 0 && (
-        <div className="text-center text-base-content/50">
-          No followed threads found.
+    <div className="mx-auto mb-24 h-full w-full max-w-5xl flex-col items-center justify-center px-6">
+      <div className="hero mt-8">
+        <div className="hero-content text-center">
+          <h1 className="mb-1 text-4xl font-bold">📚 Your Followed Threads</h1>
         </div>
-      )}
-
-      {!loading &&
-        threads.map((thread) => (
-          <ThreadCard key={thread.thread_id} thread={thread} categoryTitle="" />
-        ))}
-
-      <Pagination
-        page={page}
-        hasMore={threads.length === 5}
-        onPageChange={setPage}
-      />
+      </div>
+      <FollowedThreadList userId={userId} />
     </div>
   );
 };
-
-export default FollowedThreadList;
+export default FollowedThreads;
