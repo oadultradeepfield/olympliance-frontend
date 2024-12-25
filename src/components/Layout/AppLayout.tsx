b9ce17146daca6a47070e2669108029abad704d2
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2024-12-25 16:06:48 +0700

    reduce backdrop blur

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -8,7 +8,7 @@ interface AppLayoutProps {
 const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   return (
     <div className="flex min-h-screen flex-col">
-      <div className="sticky top-0 z-[50] border-b-2 border-base-content/15 bg-base-100/50 backdrop-blur-lg">
+      <div className="sticky top-0 z-[50] border-b-2 border-base-content/15 bg-base-100/50 backdrop-blur-md">
         <Header />
       </div>
       <div className="flex flex-grow">{children}</div>
