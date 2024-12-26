b7d53c1c92b21ac322c7e00c848a7bba9c78148f
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2024-12-26 17:27:01 +0700

    fix random category in slug

--- src/pages/Thread/ThreadPage.tsx
+++ src/pages/Thread/ThreadPage.tsx
@@ -1,17 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import ThreadInteraction from "../../components/Thread/ThreadInteraction";
 import ThreadComment from "../../components/Comment/ThreadComment";
+import { categories } from "../../data/categoriesData";
 
 const ThreadPage: React.FC = () => {
-  const { categoryTitle, id } = useParams<{
-    categoryTitle?: string;
-    id?: string;
-  }>();
+  const { category, id } = useParams<{ category?: string; id?: string }>();
+
+  const categoryExists = categories.some(
+    (cat) => cat.title.toLowerCase() === category?.toLowerCase(),
+  );
+
   const threadId = id ? Number(id) : NaN;
+
+  if (!categoryExists) {
+    return <Navigate to="/not-found" />;
+  }
 
   return (
     <div className="mx-auto mb-24 mt-2 h-full w-full max-w-5xl flex-col items-center justify-center p-6">
-      <ThreadInteraction threadId={threadId} category={categoryTitle || ""} />
+      <ThreadInteraction threadId={threadId} category={category || ""} />
       <div className="divider my-4"></div>
       <ThreadComment threadId={threadId} />
     </div>
